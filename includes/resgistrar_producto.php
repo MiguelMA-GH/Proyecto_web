03187dff330ee31127137ea453978fa7077e6523
MiguelMA-GH <al269436@uji.es>
2020-11-09 17:01:09 +0100

    Create resgistrar_producto.php
    Hay que probarlo

---
+++ includes/resgistrar_producto.php
@@ -0,0 +1,27 @@
+<?php
+
+function registrar_producto($table)
+{
+    global $pdo;
+
+    $datos = $_REQUEST;
+    if (count($_REQUEST) < 2) {
+        $data["error"] = "No has rellenado el formulario correctamente";
+        return;
+    }
+    $query = "INSERT INTO $table (nombre, imagen)
+                          VALUES (?,?)";
+    try { 
+        $consult = $pdo -> prepare($query);
+        $a = $consult->execute(array($_REQUEST['nombre'], $_REQUEST['imagen']  ));
+
+        if (1>$a) echo "<h1> Inserción incorrecta </h1>";
+        else echo "<h1> Producto registrado! </h1>";
+
+    
+    } catch (PDOExeption $e) {
+        echo ($e->getMessage());
+    }
+}
+
+?>
