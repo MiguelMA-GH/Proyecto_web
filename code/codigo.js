0bf85b386878cf841e38028b0f6ab18499a9480b
MiguelMA-GH <al269436@uji.es>
2020-11-09 16:08:00 +0100

    Canvas y estilos

--- code/codigo.js
+++ code/codigo.js
@@ -12,5 +12,14 @@ function ocultarDiv()
 
 }
 
+function handleFiles(e)	{
+    let ctx	=	document.getElementById('canvas').getContext('2d');
+    let img	=	new	Image;
+    img.src	=	URL.createObjectURL(e.target.files[0]);
+    img.onload	=	function()	{
+                    ctx.drawImage(img,	20,20);
+    }
+}
+
 
 document.getElementById('imagen').style.display='none';
--- partials/insertar_producto.php
+++ partials/insertar_producto.php
@@ -3,13 +3,17 @@
 	<form class="form_ins_prod" action="?action=insertar_producto" method="POST">
 
 		<label for="nombre">Nombre</label>
-		<br/>
+	
 		<input type="text" name="name" class="item_requerid" size="20" maxlength="25" value=""
 		/>
-		<br/>
-     
 		
-		<br/>
+		<label for="imagen">Imagen</label>
+		
+		<input type="text" name="imagen" class="item_requerid" size="20" maxlength="25" value=""
+		/>
+		
+		
+		
 		<p><input type="submit" value="Enviar">
 		<input type="reset" value="Deshacer">
 		</p>
@@ -21,9 +25,10 @@
     <div id="imagen" class="imagen">
     <button  onclick="ocultarDiv()" id="cerrar">X</button>
         <form action="?action=upload" method="post" enctype="multipart/form-data" id="form_imagen">
-            Selecciona	una	imagen:
-            <input type="file" accept="image/*" name="tmp_file" id="upload">
-            <input type="submit" value="SUBIR" name="submit">
+            <b>Selecciona	una	imagen:</b>
+            <input type="file" class="uploadFile" accept="image/*" name="tmp_file" id="upload" onchange="handleFiles(event)">
+			<canvas id="canvas" class="canvas"></canvas>
+            <input type="submit" class ="botonCentrado" value="SUBIR" name="submit">
         </form>
     </div>
 
